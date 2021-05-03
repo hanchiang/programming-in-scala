f085d9baafc033a8efd43b187226968cfc1db738
Han Chiang <yaphc@hotmail.com>
2021-05-03 21:16:14 +0800

    Set and map

---
+++ src/main/scala/basics/maps.scala
@@ -0,0 +1,14 @@
+import scala.collection.mutable
+
+// Mutable map
+val treasureMap = mutable.Map[Int, String]()
+treasureMap += (1 -> "Go to island.")
+treasureMap += (2 -> "Find big X on ground.")
+treasureMap += (3 -> "Dig.")
+println(treasureMap(2))
+
+// Immutable map
+val romanNumeral = Map(
+  1 -> "I", 2 -> "II", 3 -> "III", 4 -> "IV", 5 -> "V"
+)
+println(romanNumeral(4))
--- src/main/scala/basics/sets.scala
+++ src/main/scala/basics/sets.scala
@@ -1,3 +1,17 @@
+import scala.collection.mutable
+import scala.collection.immutable.HashSet
+
+
+// Immutable set
 var jetSet = Set("Boeing", "Airbus")
 jetSet += "Lear"
-println(jetSet.contains("Cessna"), jetSet.contains("Lear"))
+println(jetSet.contains("Cessna"), jetSet.contains("Lear"), jetSet)
+
+// Mutable set
+val movieSet = mutable.Set("Hitch", "Poltergeist")
+movieSet += "Shrek"
+println(movieSet)
+
+// HashSet
+val hashSet = HashSet("Tomatoes", "Chilies")
+println(hashSet + "Coriander")
